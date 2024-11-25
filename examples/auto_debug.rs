9b541b4bc6e00ad14fb6cccefc67d75e356ddd33
艾帅康 <2506377990@qq.com>
2024-11-25 10:31:33 +0800

    feature: support AutoDebug macro and sikp field

--- examples/auto_debug.rs
+++ examples/auto_debug.rs
@@ -2,14 +2,17 @@ use macros::AutoDebug;
 
 #[allow(dead_code)]
 #[derive(AutoDebug)]
-#[debug(skip)]
 pub struct BulkString {
+    // #[debug(skip)]
     inner: String,
+    #[debug(skip)]
+    nothing: (),
 }
 
 fn main() {
     let bulk_string = BulkString {
         inner: "hello".to_string(),
+        nothing: (),
     };
 
     println!("{:?}", bulk_string);
--- src/auto_debug.rs
+++ src/auto_debug.rs
@@ -4,40 +4,52 @@ use quote::quote;
 use syn::DeriveInput;
 
 #[derive(Debug, FromDeriveInput)]
-#[darling(attributes(debug))]
 struct AutoDebugInfo {
     ident: syn::Ident,
     generics: syn::Generics,
     data: Data<(), AutoDebugFieldsInfo>,
-
-    #[darling(default)]
-    skip: bool,
 }
+
 #[allow(dead_code)]
 #[derive(Debug, FromField)]
+#[darling(attributes(debug))]
 struct AutoDebugFieldsInfo {
     ident: Option<syn::Ident>,
+    #[darling(default)]
+    skip: bool,
 }
 
 pub(crate) fn process_auto_debug(_input: DeriveInput) -> TokenStream {
     let AutoDebugInfo {
         ident,
         generics,
-        data: Data::Struct(_fields),
-        skip,
+        data: Data::Struct(fields),
     } = AutoDebugInfo::from_derive_input(&_input).expect("解析输入失败")
     else {
         panic!("模式匹配失败")
     };
 
+    println!("{:#?}", fields);
+
+    let fields = fields.iter().filter_map(|item| {
+        if item.skip {
+            return None;
+        }
+
+        let ident = item.ident.as_ref()?;
+
+        Some(quote! {
+            d.field(stringify!(#ident), &self.#ident);
+        })
+    });
+
     quote! {
         impl #generics std::fmt::Debug for #ident #generics {
+            #[inline]
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                if #skip {
-                    return Ok(());
-                }
-
-                todo!()
+                let mut d = f.debug_struct(stringify!(#ident));
+                #(#fields)*
+                d.finish()
             }
         }
     }
